3ae7004b6e127e3fd5a0bca31cb834c88c60e573
Bhupendra Gautam <bhupendragtm777@gmail.com>
2021-03-16 16:38:11 +0545

    Netflix Series Dynamic

--- index.js
+++ index.js
@@ -10,30 +10,39 @@ function Card(props){
 
     <div className="cards">
       <div className="card ">
-        <img src="https://m.media-amazon.com/images/M/MV5BOTk2NzUyOTctZDdlMS00MDJlLTgzNTEtNzQzYjFhNjA0YjBjXkEyXkFqcGdeQXVyMjg1NDcxNDE@._V1_.jpg" alt="myPic" className="card_img"/>
+        <img src={props.imgsrc} className="card_img"/>
         <div className="card__info">
-          <span className="card__category">A Netflix Original Series.</span>
-          <h3 className="card_title"> Dark</h3>
-          <a href="https://www.netflix.com/np/title/80100172" target= "_blank">
+          <span className="card__category">{props.title}</span>
+          <h3 className="card_title">{props.sname}</h3>
+          <a href={props.href} target= "_blank">
             <button>Watch Now</button>
           </a>
         </div>
       </div>
     </div>
 
-  </> )
+  </> );
 }
 
 ReactDOM.render(
   <>
-{/* <Card
+<Card
   imgsrc="https://m.media-amazon.com/images/M/MV5BOTk2NzUyOTctZDdlMS00MDJlLTgzNTEtNzQzYjFhNjA0YjBjXkEyXkFqcGdeQXVyMjg1NDcxNDE@._V1_.jpg"
   title="Netflix Original Series."
   sname="Dark"
   href="https://www.netflix.com/np/title/80100172"
-  button="Watch Now"
-/> */}
-<Card/>
-<Card/>
+/>
+<Card
+ imgsrc="https://uproxx.com/wp-content/uploads/2016/12/bojack_horseman_s03e04_19m55s2391f.jpeg"
+  title="Netflix Original Series."
+  sname="BoJack Horseman"
+  href="https://www.youtube.com/watch?v=i1eJMig5Ik4"
+/>
+<Card
+  imgsrc="https://uproxx.com/wp-content/uploads/2020/08/ozark-bateman-garner-feat.jpg"
+  title="Netflix Original Series."
+  sname="Stranger Things"
+  href="https://www.youtube.com/watch?v=mnd7sFt5c3A"
+/>
 </>,
 document.getElementById("root"));
