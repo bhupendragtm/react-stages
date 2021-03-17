458a9c9f2778117e07cf213b19a78fa73f44c4ca
Bhupendra Gautam <bhupendragtm777@gmail.com>
2021-03-17 16:36:27 +0545

    Use State Hook

--- App.jsx
+++ App.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import {add, sub, mult, div} from "./Calc";
+import React, {useState} from 'react';
 
-function App(){
+
+const App = () => {
+  const state = useState();
+  //console.log(state);
+
+  const [count, setCount] = useState(50);
+
+
+  const IncNum = () => {
+    setCount(100);
+    console.log(state);
+    // setCount (count +1);
+    //console.log("clicked " + count++);
+  };
+
   return(
-    <>
-<ul>
-<li>The sum of two no is {add(40, 4)}</li>
-<li> {sub(40, 4)}</li>
-<li> {mult(40, 4)}</li>
-<li> {(41, 4)}</li>
-</ul>
-</>
-);
-}
+  <>
+    <h1>{count}</h1>
+    <button onClick={IncNum}>Click Me</button>
+  </>
 
+ 
+  )
+}
 export default App;
---
+++ index3.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+function Example() {
+  const [count, setCount] = useState(0);
+  
+  
+} ReactDOM.render(const [count, setCount] = useState(0);
+  <div>
+    <p>You clicked {count} times</p>
+    <button onClick={() => setCount(count + 1)}>
+      Click me
+    </button>
+  </div>, document.getElementById("root"));
