fbf333a3d92c7a0796365af18ae34a1bb21bc75d
Bhupendra Gautam <bhupendragtm777@gmail.com>
2021-03-19 15:57:27 +0545

    Form Inital Phase

--- App.jsx
+++ App.jsx
@@ -4,14 +4,18 @@ const App = () => {
 const[fullName, setFullName] = useState({
   fname : '',
   lname : '',
-});
+  email : '',
+  phone : '',
+  });
 
 const inputEvent = (event) => {
   console.log(event.target.value);
   console.log(event.target.name);
 
-  const value = event.target.value;
-  const name = event.target.name;
+  // const value = event.target.value;
+  // const name = event.target.name;
+
+  const {value, name} = event.target;
 
   setFullName((preValue) => {
   //console.log(event.target.name);
@@ -19,15 +23,34 @@ const inputEvent = (event) => {
       return{
       fname : value,
       lname : preValue.lname,
-      }
-     } else if (name === "lName")
+      email : preValue.email,
+      phone : preValue.phone,
+      };
+    } else if (name === "lName"){
       return {
-  
         fname : preValue.fname,
         lname : value,
+        email : preValue.email,
+        phone : preValue.phone,
+
+      };
+    } else if (name === "email"){
+      return {
+        fname : preValue.fname,
+        lname : preValue.lname,
+        email : value,
+        phone : preValue.phone,
+
+      };
+    } else if (name === "phone"){
+      return {
+        fname : preValue.fname,
+        lname : preValue.lname,
+        email : preValue.email,
+        phone : value,
       };
     }
-  )
+  });
   };
 
 const onSubmits = (event) => {
@@ -41,6 +64,8 @@ return(
     <form onSubmit={onSubmits}>
     <div>
       <h1>Hello, {fullName.fname} {fullName.lname} </h1>
+      <p> {fullName.email} </p>
+      <p> {fullName.phone} </p>
 
       <input type="text"
       placeholder="Enter Your Name."
@@ -55,6 +80,22 @@ return(
       onChange={inputEvent}
      value = {fullName.lname}  
       />
+      <br/>
+      <input type="email"
+      placeholder="Enter Your email."
+      name="email"
+      onChange={inputEvent}
+      value = {fullName.email}
+      autoComplete='off'
+      />
+      <br/>
+      <input type= "number"
+      placeholder= "Enter Your Phone No."
+      name= "phone"
+      onChange = {inputEvent}
+      value = {fullName.phone}  
+      />
+
       <button type="submit">Submit Me</button>
     </div>
     </form>
