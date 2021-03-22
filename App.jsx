c4fa9ed8be238b9129420ed5f66feacf3ea6e4c9
Bhupendra Gautam <bhupendragtm777@gmail.com>
2021-03-22 13:12:34 +0545

    Bootstrap in React.js

--- App.jsx
+++ App.jsx
@@ -1,106 +1,13 @@
-import React, {useState} from 'react';
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-const[fullName, setFullName] = useState({
-  fname : '',
-  lname : '',
-  email : '',
-  phone : '',
-  });
+  return(
+    <React.Fragement>
+    <h1>I Live in Kathmandu.</h1>
+    <button className="btn btn-success"> Bhupendra Gautam</button>
+    </React.Fragement>
+  );
+};
 
-const inputEvent = (event) => {
-  console.log(event.target.value);
-  console.log(event.target.name);
-
-  // const value = event.target.value;
-  // const name = event.target.name;
-
-  const {value, name} = event.target;
-
-  setFullName((preValue) => {
-  //console.log(event.target.name);
-    if(name === "fName"){
-      return{
-      fname : value,
-      lname : preValue.lname,
-      email : preValue.email,
-      phone : preValue.phone,
-      };
-    } else if (name === "lName"){
-      return {
-        fname : preValue.fname,
-        lname : value,
-        email : preValue.email,
-        phone : preValue.phone,
-
-      };
-    } else if (name === "email"){
-      return {
-        fname : preValue.fname,
-        lname : preValue.lname,
-        email : value,
-        phone : preValue.phone,
-
-      };
-    } else if (name === "phone"){
-      return {
-        fname : preValue.fname,
-        lname : preValue.lname,
-        email : preValue.email,
-        phone : value,
-      };
-    }
-  });
-  };
-
-const onSubmits = (event) => {
-  event.preventDefault();
-  alert("form submitted");
-  };
-
-return(
-    <>
-  <div className="main_div">
-    <form onSubmit={onSubmits}>
-    <div>
-      <h1>Hello, {fullName.fname} {fullName.lname} </h1>
-      <p> {fullName.email} </p>
-      <p> {fullName.phone} </p>
-
-      <input type="text"
-      placeholder="Enter Your Name."
-      name="fName"
-      onChange={inputEvent}
-      value = {fullName.fname}  
-      />
-      <br/>
-      <input type="text"
-      placeholder="Enter Your Last Name."
-      name="lName"
-      onChange={inputEvent}
-     value = {fullName.lname}  
-      />
-      <br/>
-      <input type="email"
-      placeholder="Enter Your email."
-      name="email"
-      onChange={inputEvent}
-      value = {fullName.email}
-      autoComplete='off'
-      />
-      <br/>
-      <input type= "number"
-      placeholder= "Enter Your Phone No."
-      name= "phone"
-      onChange = {inputEvent}
-      value = {fullName.phone}  
-      />
-
-      <button type="submit">Submit Me</button>
-    </div>
-    </form>
-  </div>
-</>
-)
- };
 export default App;
--- index.js
+++ index.js
@@ -3,28 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// const fullname = ['Abdul','Raheem'];
-
-// const biodata = [1, ...fullname, 26, "male"];
-
-// var shooterGames = ['Call of Duty','Far Cry','Resident Evil','Call of Duty'];
-// var racingGames = ['Need For Speed', 'Gran Turismo', 'Burnout'];
-// var games = [...shooterGames, ...racingGames];
-
-// var shooterGames1 = ['Call of Duty','Far Cry','Resident Evil','Call of Duty'];
-// var [first, ...remaining] = shooterGames1;
-
-// const fullName = {
-//     fname:"Bhupendra",
-//     lname:"Gautam",
-// };
-// const biodata = {
-//     id : 1,
-//     ...fullName,
-//     age : 26,
-//     gender : 'male',
-// };
-
-// console.log(biodata)
+ReactDOM.render(<App />,document.getElementById("root"));
